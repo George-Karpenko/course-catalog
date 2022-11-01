7b9f9e4aec63ae899da5031a3462c0e543e64cdc
GeorgeKarpenko <g.r.karpenko@yandex.ru>
2022-11-01 11:16:09 +0300

    мелочи

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import TheCourses from "./components/TheCourses.vue";
-
 </script>
 
 <template>
   <Suspense>
-    <TheCourses/>
-    <template #fallback> Loading... </template>
+    <TheCourses />
+    <template #fallback>
+      <div class="flex justify-center items-center h-screen">
+        <p>{{ $t("loading") }}...</p>
+      </div>
+    </template>
   </Suspense>
 </template>
-
